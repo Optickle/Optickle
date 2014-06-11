c704031d0a2313d18e3e89d1206f52f677e2b8a8
eoelker <ericoelker@gmail.com>
2014-06-11 16:33:13 -0400

    Wrote squeezer.m.  Fixed getFieldMatrixAC.m to use expandFieldMatrixRF from Optic class

--- @Squeezer/Squeezer.m
+++ @Squeezer/Squeezer.m
@@ -2,40 +2,53 @@ classdef Squeezer < Optic
   % Squeezer is a type of Optic used in Optickle
   %
   % Squeezers are used to     
-  % obj = Squeezer (name, loss, )
+  % obj = Squeezer (name, loss, sqAng, sqdB)
   %
-  % A sink has 1 input and 1 output.
-  % Input:  1, in, fr
-  % Output: 1, out, fr
+  % A Squeezer has 1 input and 1 output.
+  % Input:  1, in, 
+  % Output: 1, out,
   %
   % ==== Members
   % Optic - base class members
-  % loss - fraction of incident power lost going from input to output
-  %   Usually this can be left at its default value of 1.  Smaller values
-  %   can be used to make attenuators.
-  %
+  % loss - This represents the escape efficiency of the OPO 
+  %   (1-loss = escape efficiency)
+  % sqAng - squeezing angle
+  % sqdB - amount of squeezing in dB without losses (ie perfect escape
+  %  efficiency)
+  % 
   % ==== Functions, those in Optic
   %
   % Example: a sink at the REFL port
   % obj = Sink('REFL');
 
   properties
-      loss = []; % power loss going from input to output
+    loss = []; % 1-escape efficiency
+    sqAng = []; % squeezing angle
+    sqdB =[]; % level of squeezing without losses in dB
   end
   
   methods
-    function obj = Sink(name, varargin)
-    % obj = Sink(name, loss)
+    function obj = Squeezer(name, varargin)
+    % obj = Squeezer(name, loss, sqAng, sqdB)
     %
-    % Optical parameters:
-    % loss - power loss from input to output (default is 1)
+    % Parameters:
+    %
+    % loss - This represents the escape efficiency of the OPO 
+    %   (1-loss = escape efficiency)
+    % sqAng - squeezing angle
+    % sqdB - amount of squeezing in dB with perfect escape efficiency
+    %
+    % Default parameters are:
+    % [loss, sqAng, sqdB] = 
+    % [1, 0, 10]
+
         
     % deal with no arguments
     if nargin == 0
         name = '';
     end
 
-    % build optic (a sink has no drives)
+    % build optic (a Squeezer has no drives)
     inNames    = {'in'} ;
     outNames   = {'out'};
     driveNames = {};
@@ -45,7 +58,7 @@ classdef Squeezer < Optic
     errstr = 'Don''t know what to do with ';	% for argument error messages
     switch( nargin )
       case 0					% default constructor, do nothing
-      case {1 2}
+      case {1, 2, 3, 4}
         % copy constructor
         %if( isa(arg, class(obj)) )
         %  obj = arg;
@@ -53,11 +66,11 @@ classdef Squeezer < Optic
         %end
 
         % loss
-        args = {1};
+        args = {1,0,10};
         args(1:(nargin-1)) = varargin(1:end);
       
         % store stuff in class
-        [obj.loss] = deal(args{:});
+        [obj.loss, obj.sqAng, obj.sqdB] = deal(args{:});
       
       otherwise
         % wrong number of input args
--- @Squeezer/getFieldMatrixAC.m
+++ @Squeezer/getFieldMatrixAC.m
@@ -10,7 +10,8 @@ function mOptAC = getFieldMatrixAC(obj, pos, par)
   mLoss = blkdiagN(sqrt(1-obj.loss), 2*par.Nrf);
   
   % apply squeezing operator
-  r = obj.SQZdB*(ln(10)/20) %calculate squeezing factor
-  mSqz = [cosh(r), -exp(i*2*par.SqzAng)*sinh(r); -exp(-i*2*par.SqzAng)*sinh(r), cosh(r)];
-  mOptAC = obj.
-   
+  r = obj.SQZdB*(ln(10)/20); %calculate squeezing factor
+  mSqz = [cosh(r), -exp(i*2*obj.sqAng)*sinh(r); -exp(-i*2*obj.sqAng)*sinh(r), cosh(r)];
+  mOpt = mLoss*mSqz;
+  mOptAC = obj.expandFieldMatrixRF(mOpt, par.Nrf);
+ 
