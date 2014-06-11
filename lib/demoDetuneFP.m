ac188751a8cb6f42aa3cb1ef2e23f0f38cab0d43
Nicolas Smith <nicolas.d.smith@gmail.com>
2014-06-11 16:30:51 -0700

    Create a test for demoDetuneFP

--- lib/demoDetuneFP.m
+++ lib/demoDetuneFP.m
@@ -2,7 +2,11 @@
 %   this function demonstrates the use of tickle with optFP
 %
 
-function demoDetuneFP
+function outstruct = demoDetuneFP(makePlot)
+
+  if nargin < 1
+      makePlot = 1;
+  end
 
   % create the model
   opt = optFP;
@@ -21,41 +25,65 @@ function demoDetuneFP
 
   % compute the DC signals and TFs on resonance
   f = logspace(-1, 3, 200)';
-  [fDC, sigDC0, sigAC0, mMech0, noiseAC0] = tickle(opt, [], f);
+  [fDC0, sigDC0, sigAC0, mMech0, noiseAC0] = tickle(opt, [], f);
   
-  % Print out the fields and probes, just to demonstrate these functions:
-  fprintf('DC fields (fDC matrix):\n');
-  showfDC(opt, fDC);
-  
-  fprintf('\nProbes (sigDC matrix):\n');
-  showsigDC(opt, sigDC0);
+  if makePlot
+      % Print out the fields and probes, just to demonstrate these functions:
+      fprintf('DC fields (fDC matrix):\n');
+      showfDC(opt, fDC0);
+      
+      fprintf('\nProbes (sigDC matrix):\n');
+      showsigDC(opt, sigDC0);
+  end
   
   % compute the same a little off resonance
   pos = zeros(opt.Ndrive, 1);
   pos(nEX) = 0.1e-9;
-  [fDC, sigDC1, sigAC1, mMech1, noiseAC1] = tickle(opt, pos, f);
+  [fDC1, sigDC1, sigAC1, mMech1, noiseAC1] = tickle(opt, pos, f);
   
   % and a lot off resonance
   pos(nEX) = 1e-9;
-  [fDC, sigDC2, sigAC2, mMech2, noiseAC2] = tickle(opt, pos, f);
+  [fDC2, sigDC2, sigAC2, mMech2, noiseAC2] = tickle(opt, pos, f);
   
   % make a response plot
   h0 = getTF(sigAC0, nREFL_I, nEX);
   h1 = getTF(sigAC1, nREFL_I, nEX);
   h2 = getTF(sigAC2, nREFL_I, nEX);
   
-  figure(1)
-  zplotlog(f, [h0, h1, h2])
-  title('PDH Response for Detuned Cavity', 'fontsize', 18);
-  legend('On resonance', '0.1 nm', '1 nm', 'Location','SouthEast');
+  if makePlot
+      figure(1)
+      zplotlog(f, [h0, h1, h2])
+      title('PDH Response for Detuned Cavity', 'fontsize', 18);
+      legend('On resonance', '0.1 nm', '1 nm', 'Location','SouthEast');
+  end
   
   % make a noise plot
   n0 = noiseAC0(nREFL_I, :)';
   n1 = noiseAC1(nREFL_I, :)';
   n2 = noiseAC2(nREFL_I, :)';
   
-  figure(2)
-  loglog(f, abs([n0 ./ h0, n1 ./ h1, n2 ./ h2]))
-  title('Quantum Noise Limit for Detuned Cavity', 'fontsize', 18);
-  legend('On resonance', '0.1 nm', '1 nm');
-  grid on
+  if makePlot
+      figure(2)
+      loglog(f, abs([n0 ./ h0, n1 ./ h1, n2 ./ h2]))
+      title('Quantum Noise Limit for Detuned Cavity', 'fontsize', 18);
+      legend('On resonance', '0.1 nm', '1 nm');
+      grid on
+  end
+  
+  if nargout > 0
+      outstruct.fDC0 = fDC0;
+      outstruct.fDC1 = fDC1;
+      outstruct.fDC2 = fDC2;
+      outstruct.sigDC0 = sigDC0;
+      outstruct.sigDC1 = sigDC1;
+      outstruct.sigDC2 = sigDC2;
+      outstruct.h0 = h0;
+      outstruct.h1 = h1;
+      outstruct.h2 = h2;
+      outstruct.n0 = n0;
+      outstruct.n1 = n1;
+      outstruct.n2 = n2;
+      outstruct.mMech0 = mMech0;
+      outstruct.mMech1 = mMech1;
+      outstruct.mMech2 = mMech2;
+  end
