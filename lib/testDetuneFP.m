0793f59c04051f5cbd19e88b07e77533d976cc49
Nicolas Smith <nicolas.d.smith@gmail.com>
2014-06-12 12:09:20 -0700

    tests working

--- lib/testDetuneFP.m
+++ lib/testDetuneFP.m
@@ -1,13 +1,14 @@
 function exitCond = testDetuneFP()
     exitCond = 0;
-    tic
     
     referenceStruct = load('testDetuneFP.mat');
     % referenceStruct is saved with commands:
     % output = demoDetuneFP(0);
     % save('testDetuneFP.mat','-struct','output')
     
+    tic
     calculationStruct = demoDetuneFP(0);
+    toc
     
     numericalEquality = isequaln(referenceStruct,calculationStruct);
 
@@ -15,5 +16,44 @@ function exitCond = testDetuneFP()
         warning('Answers are numerically different, this may be OK if error is small')
         exitCond = exitCond+1;
     end
-    toc
+    
+    
+    %%% test DC calculations
+    exitCond = exitCond + checkForErrors('DC',{'fDC0','fDC1','fDC2','sigDC0','sigDC1','sigDC2'},...
+                   calculationStruct,referenceStruct);
+    
+    
+    %%% test TF calculations
+    exitCond = exitCond + checkForErrors('Transfer function',{'sigAC0','sigAC1','sigAC2','mMech0','mMech1','mMech2'},...
+                   calculationStruct,referenceStruct);
+    
+    %%% test Noise calculations
+    exitCond = exitCond + checkForErrors('Noise',{'noiseAC0','noiseAC1','noiseAC2'},...
+                   calculationStruct,referenceStruct);
+end
+
+
+function errorCount = checkForErrors(label, fieldNames, calcStruct, refStruct)
+    errorCount = 0;
+    errorThreshold = 1e-14;
+
+    for name = fieldNames
+        try
+            calcMat = calcStruct.(name{:});
+            refMat = refStruct.(name{:});
+            
+            errorMat = abs(1 - calcMat./refMat);
+            
+            error = max(errorMat(:));
+            
+            if error > errorThreshold
+                warning([label ' error is above threshold'])
+                errorCount = errorCount + 1;
+            end
+        catch err
+            warning(['Error occurred while calculating ' label ' calculation error: ' err.message])
+            errorCount = errorCount + 1;
+        end
+    end
+
 end
