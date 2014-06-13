4681da434d5ae3b249fe8ff4600b79dbb23644fc
m3v4n5 <Matthew Evans>
2014-06-13 10:21:33 -0400

    small edits, preparing for quantum noise change

--- @Optickle/addDriveOffset.m
+++ @Optickle/addDriveOffset.m
@@ -16,3 +16,4 @@ function opt = addDriveOffset(opt, nDrv, dx)
   pos = getPosOffset(opt);        % get all pos offsets
   pos(nDrv) = pos(nDrv) + dx;     % change some of them
   opt = setPosOffset(opt, pos);   % set all pos offsets
+end
