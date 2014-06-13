f23ff4dd514c2968a6eb2a06274c1a4e02b947f4
Nicolas Smith <nicolas.d.smith@gmail.com>
2014-06-13 16:39:43 -0700

    live testing

--- tests/optickleTestConfig.m
+++ tests/optickleTestConfig.m
@@ -1,7 +1,7 @@
 function config = optickleTestConfig()
     % referenceType can be 'Files' or 'Path'
-%     config.referenceType = 'Path';
-%     config.referencePath = '~/tmp/Optickle';
-    config.referenceType = 'Files';
-    config.referencePath = './referenceFiles';
+    config.referenceType = 'Path';
+    config.referencePath = '~/tmp/Optickle';
+    %config.referenceType = 'Files';
+    %config.referencePath = './referenceFiles';
 end
