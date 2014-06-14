2e535e7b45552bb2f46c2e733f73800f5acf00d4
Nicolas Smith <nicolas.d.smith@gmail.com>
2014-06-13 17:28:29 -0700

    can now test on arbitrary opt-producing function.

---
+++ tests/TickleTestOnOpt.m
@@ -0,0 +1,40 @@
+classdef TickleTestOnOpt < OptickleTest
+    properties 
+        optFuncHandle
+    end
+    
+    % Define the constructor method
+    methods
+        function testCase = TickleTestOnOpt(optFuncHandle) 
+            testCase.optFuncHandle = optFuncHandle;
+            
+            testCase.testFunctionHandle = @()computeResults(testCase);
+        end
+        function resultStruct = computeResults(testCase)
+            f = logspace(-1, 3, 200)';
+            
+            [fDC, sigDC, sigAC, mMech, noiseAC] = tickle(testCase.optFuncHandle(),[], f);
+            
+            resultStruct = var2struct(fDC,sigDC,sigAC,mMech,noiseAC);
+        end
+    end
+    % Exact numerical equality is automatically tested, define additional
+    % tests here
+    methods (Test)
+        function testfDC(testCase)
+            testCase.verifyCalcAndRefMatrices('fDC',{'fDC'});
+        end
+        function testsigDC(testCase)
+            testCase.verifyCalcAndRefMatrices('sigDC',{'sigDC'});
+        end
+        function testsigAC(testCase)
+            testCase.verifyCalcAndRefMatrices('sigAC',{'sigAC'});
+        end
+        function testmMech(testCase)
+            testCase.verifyCalcAndRefMatrices('mMech',{'mMech'});
+        end
+        function testnoiseAC(testCase)
+            testCase.verifyCalcAndRefMatrices('noiseAC',{'noiseAC'});
+        end
+    end
+end
