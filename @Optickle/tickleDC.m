60a0e3b87a6749086c9b164c47c0c4769518a50f
Nicolas Smith <nicolas.d.smith@gmail.com>
2014-06-16 17:34:58 -0400

    !!!WARNING!!! THIS COMMIT SHOULD BE REVERTED AFTER TESTING
    added extra transpose to agree with Optickle 1

--- @Optickle/tickleDC.m
+++ @Optickle/tickleDC.m
@@ -59,7 +59,7 @@ function [vLen, prbList, mapList, mPhiFrf, vDC, mPrb, mPrbQ] = ...
     vDCinCT = (mIn_k * vDC)';
     
     % compute the row of mPrb for this probe
-    mPrb(k, 1:Nfld) = vDCinCT * mPrb_k * mIn_k;
+    mPrb(k, 1:Nfld) = vDCinCT * mPrb_k.' * mIn_k; % JUST FOR TESTING, CHANGE BACK!
     
     % quad phase signals, for oscillator phase noise
     mPrbQ_k = prbList(k).mPrbQ;
