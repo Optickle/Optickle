9fe75e5c05e08c9cb347b0e39b34263e48a34aec
eoelker <ericoelker@gmail.com>
2014-06-17 18:09:45 -0400

    "Changed antisqueezing level"

--- debug/eoOptTestSqz.m
+++ debug/eoOptTestSqz.m
@@ -10,7 +10,7 @@ function opt = eoOptTestSqz
   opt = addSource(opt, 'Laser', 1, 0, 0);
 
   % add a Squeezer
-  opt = addSqueezer(opt, 'Sqz1', 1064e-9 , 0, 1, 0, 10, 10, 0);
+  opt = addSqueezer(opt, 'Sqz1', 1064e-9 , 0, 1, 0, 10, 15, 0);
   opt = addLink(opt, 'Laser', 'out', 'Sqz1', 'in', 0);
 
   % add a Sink
