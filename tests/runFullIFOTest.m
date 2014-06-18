b64749b954f54d284cdc7da13a41b52a06b1e973
Nicolas Smith <nicolas.d.smith@gmail.com>
2014-06-18 16:26:21 -0400

    break 01 test into separate script

--- tests/runFullIFOTest.m
+++ tests/runFullIFOTest.m
@@ -10,20 +10,11 @@ addpath('testFunctions')
 test = TickleTestOnOpt(@optFullIFO);
 results = test.run();
 
-disp(results)
-
 global optickleTestResultsFile
 load(optickleTestResultsFile);
 
-%% run the Tickle01 tests
-
-test = Tickle01TestOnOpt(@optFullIFO);
-results = test.run();
-
+%% display results
 disp(results)
-
-global optickleTestResultsFile
-data01 = load(optickleTestResultsFile);
 
 %% extract the result data
 % load the calculated data from disk, this makes variables with the names
@@ -46,14 +37,6 @@ refOMCNoise = refStruct.noiseAC(probe_OMCDC,:);
 calcPOPNoise = calcStruct.noiseAC(probe_POPI1,:);
 refPOPNoise = refStruct.noiseAC(probe_POPI1,:);
 
-%% grab the 01 data
-f = data01.refStruct.f;
-probe01 = 5;
-drive01 = 13;
-
-calcTF01 = squeeze(data01.calcStruct.sigAC(probe01,drive01,:));
-refTF01 = squeeze(data01.refStruct.sigAC(probe01,drive01,:));
-
 %% plots
 figure(333)
 subplot(2,1,1)
@@ -72,12 +55,3 @@ figure(335)
 loglog(f,refPOPNoise,f,calcPOPNoise,f,abs(refPOPNoise-calcPOPNoise))
 title('POPI1 Noise')
 legend('Reference (Optickle 1)','Calculated (Optickle 2)','Residual')
-
-%% 01 plots
-
-figure(455)
-loglog(f,abs(refTF01),f,abs(calcTF01),f,abs(refTF01-calcTF01));
-title('EX pos to OMC DC');
-legend('Reference (Optickle 1)','Calculated (Optickle 2)','Residual')
-subplot(2,1,2)
-semilogx(f,180/pi*angle(refTF01),f,180/pi*angle(calcTF01));
