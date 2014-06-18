8bfc8477c395d7bbad66ff383e897e290ee78833
m3v4n5 <m3v4n5@yahoo.com>
2014-06-18 11:23:35 -0400

    updated GouyPhase, Telescope, ... for TEM01
    getting ready for single tickle function.  merged stuff into
    getFieldMatrixAC.  Also, general cleanup of class structures.

--- @Waveplate/Waveplate.m
+++ @Waveplate/Waveplate.m
@@ -59,5 +59,44 @@ classdef Waveplate < Optic
     obj.lfw = lfw;
     obj.theta = theta;
     end
+    
+    function mOpt = getFieldMatrix(obj, pos, par)
+      % getFieldMatrix method
+      %   returns a mOpt, the field transfer matrix for this optic
+      %
+      % mOpt = getFieldMatrix(obj, pos, par)
+      
+      
+      % constants
+      Nrf = par.Nrf;
+      vNu = par.nu;
+      vPol = par.pol;
+      
+      lfw = obj.lfw;   % list of fraction of wave of the waveplate for each wavelength (e.g. 0.25 for QWP, 0.5 for HWP)
+      vfw = Optickle.mapByLambda(lfw,par.lambda);
+      theta = obj.theta / 180 * pi;   % rotation angle of the waveplate
+      mRot = [cos(theta),sin(theta);-sin(theta),cos(theta)];
+      mRotInv = inv(mRot);
+      
+      % loop over RF components
+      mOpt = zeros(Nrf, Nrf);
+      for n = 1:Nrf
+        for m = 1:Nrf
+          % wavenumber differences
+          isMatch = Optickle.isSameFreq(vNu(m), vNu(n));
+          if isMatch
+            % Jones matrix
+            mJones = mRotInv * [1,0;0,exp(-1i*2*pi*vfw(n))] * mRot;
+            for jj = 1:2
+              for kk = 1:2
+                if [vPol(n)+1,vPol(m)+1] == [jj,kk]
+                  mOpt(n,m) = mJones(jj,kk);
+                end
+              end
+            end
+          end
+        end
+      end
+    end
   end
 end
