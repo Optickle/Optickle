9f9b42b083b1f27755b8234498eafda694a1074e
Nicolas Smith <nicolas.d.smith@gmail.com>
2014-06-26 20:19:40 -0700

    set up testing against Optickle 1 reference files.

--- tests/testClasses/optickleTestConfig.m
+++ tests/testClasses/optickleTestConfig.m
@@ -1,10 +1,10 @@
 function config = optickleTestConfig()
     % referenceType can be 'Files' or 'Path'
-    config.referenceType = 'Path';
-    config.referencePath = '~/tmp/Optickle';
-    %config.referenceType = 'Files';
-    %config.referencePath = './referenceFiles';
-    config.calculationPath = '~/repos/Optickle';
+    %config.referenceType = 'Path';
+    %config.referencePath = '~/tmp/Optickle';
+    config.referenceType = 'Files';
+    config.referencePath = './referenceFiles/Optickle1';
+    config.calculationPath = '~/tmp/Optickle';
     config.residualSize = 5e-5;
     config.errorThreshold = 1e-6;
 end
