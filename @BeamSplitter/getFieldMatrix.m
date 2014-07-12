ebeae6d9fe66b7a31aac6ee72bc1fabe563e21fa
m3v4n5 <m3v4n5@yahoo.com>
2014-07-12 00:59:24 -0400

    Sagnac still winning!

--- @BeamSplitter/getFieldMatrix.m
+++ @BeamSplitter/getFieldMatrix.m
@@ -2,6 +2,10 @@
 %   returns a mOpt, the field transfer matrix for this optic
 %
 % [mOpt, mDirIn, mDirOut, dldx] = getFieldMatrix(obj, pos, par)
+%
+% Example:
+% par = opt.getOptParam;
+% [mOpt, mDirIn, mDirOut, dldx] = opt.optic{nBS}.getFieldMatrix(0, par);
 
 function [mOpt, mOptMir, mDirIn, mDirOut, dldx] = ...
   getFieldMatrix(obj, pos, par, varargin)
